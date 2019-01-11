21d0a5c0e95abfad6be8727174c2819a0ecf0e28
Bendegúz Németh <n.bendeguz3@gmail.com>
2019-01-11 14:52:34 +0100

    Created View Controllers and Basic UI

---
+++ Weather/Weather/CitiesListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CitiesListViewController.swift
+//  Weather
+//
+//  Created by Németh Bendegúz on 2019. 01. 11..
+//  Copyright © 2019. Németh Bendegúz. All rights reserved.
+//
+
+import UIKit
+
+class CitiesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
