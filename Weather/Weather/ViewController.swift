ca5ec5e10e152a404322e8a2456aeec54cfa2a70
Bendegúz Németh <n.bendeguz3@gmail.com>
2019-01-11 13:32:00 +0100

    Initial Commit

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Németh Bendegúz on 2019. 01. 11..
+//  Copyright © 2019. Németh Bendegúz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
